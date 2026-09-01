e91e6cde4c78d201f9deb70acd6068c91c2ff62c
Leodav07 <lopezvelasquezleonardodavid@gmail.com>
2026-08-31 19:33:30 -0600

    CRUD presupuestos corregido, creado CRUD de subcategorias

--- database/procedures/p_crud_presupuestos.sql
+++ database/procedures/p_crud_presupuestos.sql
@@ -7,19 +7,36 @@ DELIMITER $$
 CREATE PROCEDURE sp_insertar_presupuesto(IN p_usuario_dni VARCHAR(18),
 										 IN p_nombre VARCHAR(40),
                                          IN p_descripcion VARCHAR(200),
-										 IN p_anio_inicio DATE,
-                                         IN p_mes_inicio DATE,
-                                         IN p_anio_final DATE,
-                                         IN p_mes_final DATE,
+										 IN p_anio_inicio MEDIUMINT,
+                                         IN p_mes_inicio TINYINT,
+                                         IN p_anio_final MEDIUMINT,
+                                         IN p_mes_final TINYINT,
                                          IN p_total_ingresos DECIMAL(8,2),
                                          IN p_total_gastos DECIMAL(8,2),
-                                         IN p_total_ahorro DECIMAL(8,2))
+                                         IN p_total_ahorro DECIMAL(8,2),
+										 IN p_creado_por VARCHAR(100))
 
 BEGIN 
+	IF p_anio_inicio > p_anio_final THEN
+		SIGNAL SQLSTATE '45004' SET MESSAGE_TEXT = 'El año final no puede ser menor que el año de inicio.';
+	END IF;
+    
+    IF p_anio_inicio = p_anio_final THEN
+		IF p_mes_inicio > p_mes_final THEN
+			SIGNAL SQLSTATE '45005' SET MESSAGE_TEXT = 'El mes final no puede ser menor que el mes de inicio.';
+		END IF;
+	END IF;
+    
+    IF EXISTS (SELECT 1 FROM presupuestos WHERE usuario_dni = p_usuario_dni AND estado = 'activo'
+					AND (anio_inicio * 100 + mes_inicio) <= (p_anio_final * 100 + p_mes_final) 
+                    AND (anio_fin * 100 + mes_fin) >= (p_anio_inicio * 100 + p_mes_inicio)) THEN
+                    SIGNAL SQLSTATE '45006' SET MESSAGE_TEXT = 'No se puede crear presupuesto ya que se traslapa con otro presupuesto activo.';
+			END IF;
+            
 	INSERT INTO presupuestos (usuario_dni, nombre, descripcion, anio_inicio, mes_inicio, anio_fin, mes_fin,
-							total_ingresos, total_gastos, total_ahorro, estado)
+							total_ingresos, total_gastos, total_ahorro, estado, creado_por)
 	VALUES (p_usuario_dni, p_nombre, p_descripcion, p_anio_inicio, p_mes_inicio, p_anio_final, p_mes_final, p_total_ingresos,
-			p_total_gastos, p_total_ahorro, 'activo');
+			p_total_gastos, p_total_ahorro, 'activo', p_creado_por);
 END $$
 
 DELIMITER ;
@@ -31,13 +48,14 @@ DROP PROCEDURE IF EXISTS sp_actualizar_presupuesto;
 
 DELIMITER $$
 
-CREATE PROCEDURE sp_actualizar_presupuesto(IN p_id_presupuesto INT,
+CREATE PROCEDURE sp_actualizar_presupuesto(IN p_usuario_dni VARCHAR(18),
+										IN p_id_presupuesto INT,
 										 IN p_nombre VARCHAR(40),
                                          IN p_descripcion VARCHAR(200),
-										 IN p_anio_inicio DATE,
-                                         IN p_mes_inicio DATE,
-                                         IN p_anio_final DATE,
-                                         IN p_mes_final DATE,
+										 IN p_anio_inicio MEDIUMINT,
+                                         IN p_mes_inicio TINYINT,
+                                         IN p_anio_final MEDIUMINT,
+                                         IN p_mes_final TINYINT,
                                          IN p_total_ingresos DECIMAL(8,2),
                                          IN p_total_gastos DECIMAL(8,2),
                                          IN p_total_ahorro DECIMAL(8,2),
@@ -45,8 +63,26 @@ CREATE PROCEDURE sp_actualizar_presupuesto(IN p_id_presupuesto INT,
 										 IN p_modificado_por VARCHAR(100))
 
 BEGIN 
+IF p_anio_inicio > p_anio_final THEN
+		SIGNAL SQLSTATE '45004' SET MESSAGE_TEXT = 'El año final no puede ser menor que el año de inicio.';
+	END IF;
+    
+    IF p_anio_inicio = p_anio_final THEN
+		IF p_mes_inicio > p_mes_final THEN
+			SIGNAL SQLSTATE '45005' SET MESSAGE_TEXT = 'El mes final no puede ser menor que el mes de inicio.';
+		END IF;
+	END IF;
+    
+    IF EXISTS (SELECT 1 FROM presupuestos WHERE usuario_dni = p_usuario_dni AND estado = 'activo' 
+					AND p_id_presupuesto != id_presupuesto
+					AND (anio_inicio * 100 + mes_inicio) <= (p_anio_final * 100 + p_mes_final) 
+                    AND (anio_fin * 100 + mes_fin) >= (p_anio_inicio * 100 + p_mes_inicio)) THEN
+                    SIGNAL SQLSTATE '45006' SET MESSAGE_TEXT = 'No se puede actualizar presupuesto ya que se traslapa con otro presupuesto activo.';
+			END IF;
+            
 	UPDATE presupuestos
-    SET usuario_dni = p_usuario_dni, nombre = p_nombre, descripcion = p_descripcion, anio_fin = p_anio_final, mes_fin = p_mes_final, 
+    SET nombre = p_nombre, descripcion = p_descripcion, anio_inicio = p_anio_inicio, mes_inicio = p_mes_inicio, 
+			anio_fin = p_anio_final, mes_fin = p_mes_final, 
             total_ingresos = p_total_ingresos, total_gastos = p_total_gastos, total_ahorro = p_total_ahorro, estado = p_estado,
             modificado_por = p_modificado_por
 	WHERE id_presupuesto = p_id_presupuesto;
@@ -89,3 +125,19 @@ DELIMITER ;
 
 
 -- 5. sp listar presupuesto de usuario
+
+DROP PROCEDURE IF EXISTS sp_listar_presupuestos_usuario;
+
+DELIMITER $$
+
+CREATE PROCEDURE sp_listar_presupuestos_usuario(IN p_usuario_dni VARCHAR(18), IN p_estado VARCHAR(20))
+BEGIN 
+	SELECT id_presupuesto, usuario_dni, nombre, descripcion, anio_inicio, mes_inicio, anio_fin, mes_fin, total_ingresos, 
+			total_gastos, total_ahorro, fecha_creacion, estado
+	FROM presupuestos
+    WHERE usuario_dni = p_usuario_dni AND 
+		(p_estado IS NULL OR p_estado = estado);
+END $$
+
+DELIMITER ;
+
---
+++ database/procedures/p_crud_subcategorias.sql
@@ -0,0 +1,96 @@
+-- 1. sp insertar subcategoria
+
+DROP PROCEDURE IF EXISTS sp_insertar_subcategoria;
+
+DELIMITER $$
+
+CREATE PROCEDURE sp_insertar_subcategoria(IN p_id_categoria INT,
+									IN p_nombre VARCHAR(50),
+									IN p_descripcion VARCHAR(255),
+                                    IN p_creado_por VARCHAR(100))
+BEGIN
+	INSERT INTO subcategorias (id_categoria, nombre, descripcion, estado, por_defecto,
+						  creado_por)
+    VALUES (p_id_categoria, p_nombre, p_descripcion, true, false, p_creado_por);
+END $$
+
+DELIMITER ;
+
+-- 2. sp actualizar subcategoria
+
+DROP PROCEDURE IF EXISTS sp_actualizar_subcategoria;
+
+DELIMITER $$
+
+CREATE PROCEDURE sp_actualizar_subcategoria(IN p_id_subcategoria INT,
+										 IN p_nombre VARCHAR(50),
+                                         IN p_descripcion VARCHAR(255),
+										 IN p_estado BOOLEAN,
+										 IN p_modificado_por VARCHAR(100))
+
+BEGIN 
+	UPDATE subcategorias
+    SET nombre = p_nombre, descripcion = p_descripcion, estado = p_estado, modificado_por = p_modificado_por
+	WHERE id_subcategoria = p_id_subcategoria;
+END $$
+
+DELIMITER ;
+
+
+-- 3. sp eliminar subcategoria
+
+DROP PROCEDURE IF EXISTS sp_eliminar_subcategoria;
+
+DELIMITER $$
+
+CREATE PROCEDURE sp_eliminar_subcategoria(IN p_id_subcategoria INT)
+
+BEGIN 
+	IF EXISTS (SELECT 1 FROM presupuestos_detalles WHERE id_subcategoria = p_id_subcategoria) 
+				OR EXISTS (SELECT 1 FROM transacciones WHERE id_subcategoria = p_id_subcategoria) THEN
+				SIGNAL SQLSTATE '45003' SET MESSAGE_TEXT = 'No es posible eliminar esta categoria ya que esta en uso en presupuestos o transacciones.';
+			ELSE
+				DELETE FROM subcategorias WHERE id_subcategoria = p_id_subcategoria;
+	END IF;
+		
+END $$
+
+DELIMITER ;
+
+
+-- 4. consultar subcategoria especifica
+DROP PROCEDURE IF EXISTS sp_consultar_subcategoria;
+
+DELIMITER $$
+
+CREATE PROCEDURE sp_consultar_subcategoria(IN p_id_subcategoria INT)
+
+BEGIN 
+	SELECT c.id_categoria, c.nombre AS nombre_categoria, c.descripcion AS descripcion_categoria, c.tipo, sc.nombre AS nombre_subcategoria,
+    sc.descripcion AS descripcion_subcategoria, sc.estado, sc.por_defecto
+    FROM categorias c 
+    INNER JOIN subcategorias sc ON sc.id_categoria = c.id_categoria 
+    WHERE sc.id_subcategoria = p_id_subcategoria;
+END $$
+
+DELIMITER ;
+
+
+-- 5. sp listar subcategorias por categoria
+
+DROP PROCEDURE IF EXISTS sp_listar_subcategorias_por_categoria;
+
+DELIMITER $$
+
+CREATE PROCEDURE sp_listar_subcategorias_por_categoria(IN p_id_categoria INT)
+
+BEGIN 
+	SELECT c.id_categoria, c.nombre AS nombre_categoria, c.descripcion AS descripcion_categoria, c.tipo, sc.nombre AS nombre_subcategoria,
+    sc.descripcion AS descripcion_subcategoria, sc.estado, sc.por_defecto
+    FROM categorias c 
+    INNER JOIN subcategorias sc ON sc.id_categoria = c.id_categoria
+    WHERE c.id_categoria = p_id_categoria;
+ 
+END $$
+
+DELIMITER ;
